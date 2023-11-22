082b89aa208dbbc1d3f62d28925d4783ad4802db
BananaF1x <brovkoman@gmail.com>
2023-11-22 15:20:32 +0200

    Додав перевірку числа

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
-const len = +prompt("Введіть довжину масива.");
+const len = +prompt("Введіть довжину масива (не менше 1).");
 let arr = [];
-for(let i = 0; i < len; i++) {
-    arr.push(prompt("Введіть елемент."));
+if(isNaN(len) != true && len > 0) {
+    for(let i = 0; i < len; i++) {
+        arr.push(prompt("Введіть елемент."));
+    }
+    document.write(`Створений масив <br> ${arr} <br>`);
+    arr.sort((a, b) => a - b);
+    document.write(`Відсортований масив за зростанням <br> ${arr} <br>`);
+    arr.splice(1, 3);
+    document.write(`Масив після видалення елементів з 2 по 4 (включно!) <br> ${arr} <br>`);
+} else {
+    document.write("Ви ввели не коректне число");
 }
-document.write(`Створений масив <br> ${arr}. <br>`);
-arr.sort((a, b) => a - b);
-document.write(`Відсортований масив за зростанням <br> ${arr}. <br>`);
-arr.splice(1, 3);
-document.write(`Масив після видалення елементів з 2 по 4 (включно!). <br> ${arr} <br>`);
